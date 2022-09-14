0bfae89b3cfe311cbda4272baed125db4d68cd88
lucas <lucas@rxc.co.kr>
2022-09-14 18:12:31 +0900

    feat: type 추론 강화 (#FE-00)

--- src/register/constants/register.ts
+++ src/register/constants/register.ts
@@ -1,7 +1,8 @@
+import { RegisterFormFields } from '../types/register';
 /**
  * 회원가입 기본 데이터
  */
-export const defaultFieldRegisterValues = {
+export const defaultFieldRegisterValues: RegisterFormFields = {
   firstName: '',
   lastName: '',
   email: '',
