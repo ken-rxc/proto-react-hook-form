4ad11b48f096f3112e37cbf5fef99495802ad76b
lucas <lucas@rxc.co.kr>
2022-09-14 18:12:31 +0900

    feat: type 추론 강화 (#FE-00)

--- src/register/utils/validation.ts
+++ src/register/utils/validation.ts
@@ -1,9 +1,10 @@
-import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { RegisterFormFields } from '../types/register';
 
 export const registerValidation = () =>
   yupResolver(
-    yup.object().shape({
+    yup.object().shape<Partial<Record<keyof RegisterFormFields, any>>>({
       firstName: yup.string().required('이름을 입력하세요.'),
       lastName: yup.string().required('성을 입력하세요.'),
       email: yup.string().email().required('이메일 주소를 입력하세요.'),
