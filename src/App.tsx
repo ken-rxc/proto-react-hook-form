fee8edb2047a4b4cf5be3bba05325d0b6dca0587
JG <dlworms@kakao.com>
2022-09-14 12:06:08 +0900

    add register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,110 @@
 import React from 'react';
-import { useForm, useWatch, Control } from "react-hook-form";
+import { useForm, Controller, FormProvider } from 'react-hook-form';
+import { Grid, TextField, Button } from '@material-ui/core';
 import './App.css';
 
-type FormValues = {
+/**
+ * 회원가입 데이터 타입
+ */
+interface FormValues {
   firstName: string;
   lastName: string;
-};
-
-function IsolateReRender({ control }: { control: Control<FormValues> }) {
-  const firstName = useWatch({
-    control,
-    name: "firstName",
-    defaultValue: "default"
-  });
-
-  return <div>{firstName}</div>;
+  email: string;
 }
+
+/**
+ * 회원가입 기본 데이터
+ */
+const defaultFormValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+};
 
 function App() {
   const { register, control, handleSubmit } = useForm<FormValues>();
-  const onSubmit = handleSubmit((data) => console.log(JSON.stringify(data)));
+  const formMethods = useForm<FormValues>({
+    defaultValues: defaultFormValues,
+  });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+  const handleReset = () => {
+    console.log('Reset!');
+  };
 
   return (
     <div className="App">
-      <h1>React Hook Form - Control</h1>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>First Name</label>
-          <input {...register("firstName")} />
-        </div>
-        <div>
-          <label>Last Name</label>
-          <input {...register("lastName")} />
-        </div>
-        <IsolateReRender control={control} />
+      {/* header */}
+      <h1>회원가입</h1>
 
-        <input type="submit" />
-      </form>
+      {/* form */}
+      <FormProvider {...formMethods}>
+        <form onSubmit={onSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Controller
+                control={control}
+                name="firstName"
+                defaultValue=""
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="First Name"
+                    variant="outlined"
+                    placeholder="이름을 입력하세요."
+                    fullWidth
+                    required
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                control={control}
+                name="lastName"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="Last Name"
+                    variant="outlined"
+                    placeholder="성을 입력하세요."
+                    fullWidth
+                    required
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                control={control}
+                name="email"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="E-mail"
+                    variant="outlined"
+                    placeholder="이메일 주소를 입력하세요."
+                    fullWidth
+                    required
+                  />
+                )}
+              />
+            </Grid>
+
+            {/* cta */}
+            <Grid item xs={6}>
+              <Button type="reset" variant="outlined" color="primary" size="large" fullWidth onClick={handleReset}>
+                초기화
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button type="submit" variant="contained" color="primary" size="large" fullWidth>
+                완료
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </FormProvider>
     </div>
   );
 }
